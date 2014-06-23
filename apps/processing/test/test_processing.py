90cf0afacd4e7584cecbb37bd0cc4f29b79db011
clerima <marco.clerici@jrc.ec.europa.eu>
2014-06-23 11:13:16 +0200

    Ruffus implementation

--- apps/processing/test/test_processing.py
+++ apps/processing/test/test_processing.py
@@ -2,8 +2,10 @@ __author__ = "Marco Clerici"
 
 
 import time
-from apps.processing import processing
+from apps.processing.processing import *
 start = time.clock()
+
+processing_fewsnet_rfe()
 
 elapsed = time.clock()-start
 print elapsed
