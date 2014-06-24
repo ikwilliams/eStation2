0d191344401c7a86c7ec6aedd03168081abd4fab
Jurvtk <jurriaan.van-t-Klooster@ext.jrc.ec.europa.eu>
2014-06-24 17:08:50 +0200

    Flask integration

---
+++ apps/gui/__init__.py
@@ -0,0 +1,10 @@
+#!flask/bin/python
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello Jurvtk!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
