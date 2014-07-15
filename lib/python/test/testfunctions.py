58f0f4686878a4ef6e30d070f5e947a41e53c45f
clerima <marco.clerici@jrc.ec.europa.eu>
2014-07-15 12:10:58 +0200

    Test date_time functions (to be renamed: test_functions_date.py)

--- lib/python/test/testfunctions.py
+++ lib/python/test/testfunctions.py
@@ -1,53 +1,50 @@
+from unittest import TestCase
+
 __author__ = "Jurriaan van 't Klooster"
 
 import lib.python.functions as f
 
 
-string_date = '20110506'
-result = f.is_date_yyyymmdd(string_date)
-print "is_date_yyyymmdd: %s = %s" % (string_date, result)
+class TestFunctionsDate(TestCase):
 
-string_date = '201105061330'
-result = f.is_date_yyyymmddhhmm(string_date)
-print "is_date_yyyymmddhhmm: %s = %s" % (string_date, result)
+    str_yyyy = '2011'
+    str_month = '05'
+    str_day = '01'
+    str_hh = '13'
+    str_mm = '30'
+    str_doy = '121'
+    str_dkx = '1'
 
-string_date = '2011167'
-result = f.is_date_yyyydoy(string_date)
-print "is_date_yyyydoy: %s = %s" % (string_date, result)
+    julian_dekad = 1129
+    julian_month = 377
 
-year_month_day = '20110506'
-result = f.conv_date_2_dekad(year_month_day)
-print "conv_date_2_dekad: %s = %s" % (year_month_day, result)
+    string_mmdd = str_month+str_day
+    string_yyyymmdd = str_yyyy+str_month+str_day
+    string_yyyymmddhhmm = string_yyyymmdd+str_hh+str_mm
+    string_yyyydoy = str_yyyy+str_doy
+    string_yymmk = str_yyyy[2:4]+str_month+str_dkx
+    string_yyyy_mm_dkx = str_yyyy+'_'+str_month+'_'+str_dkx
 
-year_month_day = '20110506'
-result = f.conv_date_2_month(year_month_day)
-print "conv_date_2_month: %s = %s" % (year_month_day, result)
+    def test_is_date_time(self):
 
-dekad = '21'
-result = f.conv_dekad_2_date(dekad)
-print "conv_dekad_2_date: %s = %s" % (dekad, result)
-
-month = '6'
-result = f.conv_month_2_date(month)
-print "conv_month_2_date: %s = %s" % (month, result)
-
-year = '2001'
-doy = '267'
-yeardoy = year+doy
-result = f.conv_date_yyyydoy_2_yyyymmdd(yeardoy)
-print "conv_date_yyyydoy_2_yyyymmdd: year: %s doy: %s = %s" % (year, doy, result)
-
-year_month_day = '20110506'
-result = f.conv_date_yyyymmdd_2_doy(year_month_day)
-print "conv_date_yyyymmdd_2_doy: %s = %s" % (year_month_day, result)
-
-yyyy_mm_dkx = '2001_06_dk2'
-result = f.conv_yyyy_mm_dkx_2_yyyymmdd(yyyy_mm_dkx)
-print "conv_yyyy_mm_dkx_2_yyyymmdd: %s = %s" % (yyyy_mm_dkx, result)
-
-yymmk = '06061'
-result = f.conv_yymmk_2_yyyymmdd(yymmk)
-print "conv_yymmk_2_yyyymmdd: %s = %s" % (yymmk, result)
+        self.assertTrue(f.is_date_yyyymmdd(self.string_yyyymmdd))
+        self.assertTrue(f.is_date_mmdd(self.string_mmdd))
+        self.assertTrue(f.is_date_yyyymmddhhmm(self.string_yyyymmddhhmm))
+        self.assertTrue(f.is_date_yyyydoy(self.string_yyyydoy))
 
 
+    def test_convert_date_time(self):
 
+        self.assertEqual(f.conv_date_2_dekad(self.string_yyyymmdd), self.julian_dekad)
+        self.assertEqual(f.conv_date_2_month(self.string_yyyymmdd), self.julian_month)
+
+        self.assertEqual(f.conv_dekad_2_date(self.julian_dekad), self.string_yyyymmdd)
+        self.assertEqual(f.conv_month_2_date(self.julian_month), self.string_yyyymmdd)
+
+        self.assertEqual(f.conv_date_yyyydoy_2_yyyymmdd(self.string_yyyydoy),self.string_yyyymmdd)
+        self.assertEqual(f.conv_date_yyyymmdd_2_doy(self.string_yyyymmdd), int(self.str_doy))
+
+        self.assertEqual(f.conv_yyyy_mm_dkx_2_yyyymmdd(self.string_yyyy_mm_dkx), self.string_yyyymmdd)
+        self.assertEqual(f.conv_yymmk_2_yyyymmdd(self.string_yymmk), self.string_yyyymmdd)
+
+#
