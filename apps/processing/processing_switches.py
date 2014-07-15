41fa88bdc9daebed1a4e215ecd08c1bb8f54aa80
clerima <marco.clerici@jrc.ec.europa.eu>
2014-07-15 17:07:10 +0200

    New file for controlling which processing chains to run (to be done ultimately from the DB)

---
+++ apps/processing/processing_switches.py
@@ -0,0 +1,10 @@
+activate_vgt_ndvi_comput=0
+activate_fewsnet_rfe_comput=1
+
+#   If == 0, no run
+#   If >= 1, run run with verbose = level
+
+pipeline_run_level=1
+pipeline_run_touch_only=0
+pipeline_printout_level=4
+pipeline_printout_graph_level=0
