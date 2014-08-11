7ef0544218021019a75627773da33a5750a8cc72
clerima <marco.clerici@jrc.ec.europa.eu>
2014-08-11 15:23:41 +0200

    Change working case

--- apps/processing/processing_switches.py
+++ apps/processing/processing_switches.py
@@ -4,7 +4,7 @@ activate_fewsnet_rfe_comput=1
 #   If == 0, no run
 #   If >= 1, run run with verbose = level
 
-pipeline_run_level=1
+pipeline_run_level=0
 pipeline_run_touch_only=0
-pipeline_printout_level=4
+pipeline_printout_level=10
 pipeline_printout_graph_level=0
