c939b1834cc10936b27c3d12548640b007958853
clerima <marco.clerici@jrc.ec.europa.eu>
2014-08-12 17:51:26 +0200

    Group the metadata in categories, and complete the list

--- lib/python/metadata.py
+++ lib/python/metadata.py
@@ -10,59 +10,98 @@ import datetime
 # Import eStation2 modules
 from lib.python import es_logging as log
 from lib.python.functions import get_subdir_from_path_full
+from database.querydb import get_product_out_info
 logger = log.my_logger(__name__)
 
-sds_metadata = { 'eStation2_product': '',
-                 'eStation2_subProduct': '',
-                 'eStation2_version': '',
-                 'eStation2_es2_version': '',
-                 'eStation2_mapset': '',
-                 'eStation2_comp_time': '',
-                 'eStation2_input_files': '',
-                 'eStation2_scaling_factor': '',
-                 'eStation2_scaling_offset': '',
-                 'eStation2_conversion': '',
-                 'eStation2_unit': '',
-                 'eStation2_nodata': '',
-                 'eStation2_subdir': ''
+# TODO-M.C.: Add all the attributes of 'mapset' and 'category_id' ? so that the contents of the db tables can be created (if not existing on the target station) from metadata ?
+
+sds_metadata = { 'eStation2_es2_version': '',               # 0. eStation 2 version (the fields below might depend on es2_version)
+
+                                                            # ------------------  Mapset        ----------------------
+                 'eStation2_mapset': '',                    # 1. Mapsetcode
+
+                                                            # ------------------  As in the 'product' table ----------------------
+                 'eStation2_product': '',                   # 2. productcode
+                 'eStation2_subProduct': '',                # 3. subproductcode
+                 'eStation2_version': '',                   # 4. product version (e.g. MODIS Collection 4 or 5; by default is undefined -> '')
+
+                 'eStation2_defined_by': '',                # 5. JRC or User
+                 'eStation2_category': '',                  # 6. Product category (TODO-M.C.: double-check wrt INSPIRE)
+                 'eStation2_descr_name': '',                # 7. Product Descriptive Name
+                 'eStation2_description': '',               # 8. Product Description
+                 'eStation2_provider': '',                  # 9. Product provider (NASA, EUMETSAT, VITO, ..)
+
+                 'eStation2_date_format': '',               # 10. Date format (YYYYMMDDHHMM, YYYYMMDD or MMDD)
+                 'eStation2_frequency': '',                 # 11. Product frequency (as in db table 'frequency')
+
+                 'eStation2_scaling_factor': '',            # 12. Scaling factors
+                 'eStation2_scaling_offset': '',            # 13. Scaling offset
+                 'eStation2_unit': '',                      # 14. physical unit (none for pure numbers, e.g. NDVI)
+                 'eStation2_nodata': '',                    # 15. nodata value
+                 'eStation2_subdir': '',                    # 16. subdir in the eStation data tree (redundant ??)
+
+                                                            # ------------------  File Specific ----------------------
+                 'eStation2_date': '',                      # 17. Date of the product
+
+                                                            # ------------------  File/Machine Specific ----------------------
+                 'eStation2_input_files': '',               # 18. Input files used for computation
+                 'eStation2_comp_time': '',                 # 19. Time of computation
+
+                                                            # ------------------  Fixed         ----------------------
+                 'eStation2_conversion': ''                 # 20. Rule for converting DN to physical values (free text)
+
 }
 
 class SdsMetadata:
 
     def __init__(self):
 
+        sds_metadata['eStation2_es2_version'] = 'my_eStation2_sw_release'
+
+        sds_metadata['eStation2_mapset'] = 'my_mapset_code'
+
         sds_metadata['eStation2_product'] = 'my_product'
         sds_metadata['eStation2_subProduct'] = 'my_sub_product'
         sds_metadata['eStation2_version'] = 'my_product_version'
-        sds_metadata['eStation2_es2_version'] = 'my_eStation2_sw_release'
-        sds_metadata['eStation2_mapset'] = 'my_mapset_short_name'
-        sds_metadata['eStation2_comp_time'] = '2014-01-01 12:00:00'
-        sds_metadata['eStation2_input_files'] = '/my/path/to/file/and/filename1'
+
+        sds_metadata['eStation2_defined_by'] = 'JRC'
+        sds_metadata['eStation2_category'] = 'my_product_category'
+        sds_metadata['eStation2_descr_name'] = 'my_descriptive_name'
+        sds_metadata['eStation2_description'] = 'my_description'
+        sds_metadata['eStation2_provider'] = 'my_product_provider'
+
+        sds_metadata['eStation2_date_format'] = 'YYYYMMDDHHMM'
+        sds_metadata['eStation2_frequency'] = 'my_frequency'
+
+        sds_metadata['eStation2_conversion'] = 'Phys = DN * scaling_factor + scaling_offset'
         sds_metadata['eStation2_scaling_factor'] = 'my_scaling_factor'
         sds_metadata['eStation2_scaling_offset'] = 'my_scaling_offset'
-        sds_metadata['eStation2_conversion'] = 'Phys = DN * scaling_factor + scaling_offset'
+        sds_metadata['eStation2_unit'] = 'my_unit'
         sds_metadata['eStation2_nodata'] = 'my_nodata'
         sds_metadata['eStation2_subdir'] = 'my_subdir'
-        sds_metadata['eStation2_unit'] = 'my_unit'
+
+        sds_metadata['eStation2_date'] = '201001011200'
+        sds_metadata['eStation2_input_files'] = '/my/path/to/file/and/filename1'
+        sds_metadata['eStation2_comp_time'] = '2014-01-01 12:00:00'
 
     def write_to_ds(self, dataset):
     #
-    #   Writes to output file std metadata structure
+    #   Writes  metadata to a target file (already opened gdal dataset)
     #   Args:
     #       dataset: osgeo.gdal dataset (open and georeferenced)
 
         # TODO-M.C.: Check dataset is open
-        # TODO-M.C.: Write to a specific domain
-
-        # domain = 'eStation2'
+        # TODO-M.C.: Is it possible to write to a specific domain (e.g. 'eStation2' ???)
+        #            FTTB we use the 'eStation2_' prefix
 
         # Go through the metadata list and write to sds
         for key, value in sds_metadata.iteritems():
-            dataset.SetMetadataItem(key, value, )
+            print key, value
+            dataset.SetMetadataItem(key, str(value))
 
     def read_from_ds(self, dataset):
     #
-    #   Read std metadata structure from a file
+    #   Read metadata structure from a source file
     #   Args:
     #       dataset: osgeo.gdal dataset (open and georeferenced)
 
@@ -74,7 +113,7 @@ class SdsMetadata:
             except:
                 logger.error('Error in reading metadata item %s' % key)
 
-    def assign_time_now(self):
+    def assign_comput_time_now(self):
     #
     #   Assign current time to 'comp_time'
 
@@ -82,8 +121,10 @@ class SdsMetadata:
         str_datetime=curr_time.strftime("%Y-%m-%d %H:%M:%S")
         sds_metadata['eStation2_comp_time']=str_datetime
 
-    def assign_product(self, product, subproduct, version):
+    def assign_from_product(self, product, subproduct, version):
     #
+        product_out_info = get_product_out_info(productcode=product,subproductcode=subproduct,version=version, echo=True)
+
     #   Assign prod/subprod/version
         sds_metadata['eStation2_product'] = str(product)
         sds_metadata['eStation2_subProduct'] = str(subproduct)
@@ -92,10 +133,29 @@ class SdsMetadata:
         else:
             sds_metadata['eStation2_version'] = 'undefined'
 
-    def assign_mapset(self, mapset_short_name):
+        sds_metadata['eStation2_defined_by'] = product_out_info.defined_by
+        sds_metadata['eStation2_category'] = product_out_info.category_id
+        sds_metadata['eStation2_descr_name'] = product_out_info.descriptive_name
+        sds_metadata['eStation2_description'] = product_out_info.description
+        sds_metadata['eStation2_provider'] = product_out_info.provider
+        sds_metadata['eStation2_date_format'] = product_out_info.date_format
+        sds_metadata['eStation2_frequency'] = product_out_info.frequency_id
+        sds_metadata['eStation2_scaling_factor'] = product_out_info.scale_factor
+        sds_metadata['eStation2_scaling_offset'] = product_out_info.scale_offset
+        sds_metadata['eStation2_unit'] = product_out_info.unit
+        sds_metadata['eStation2_nodata'] = product_out_info.nodata
+
+
+    def assign_date(self, date_format, date):
+    #
+    #   Assign prod/subprod/version
+        sds_metadata['eStation2_date_format'] = str(date_format)
+        sds_metadata['eStation2_date'] = str(date)
+
+    def assign_mapset(self, mapset_code):
     #
     #   Assign mapset
-        sds_metadata['eStation2_mapset'] = str(mapset_short_name)
+        sds_metadata['eStation2_mapset'] = str(mapset_code)
 
     def assign_subdir_from_fulldir(self, full_directory):
     #
@@ -114,12 +174,13 @@ class SdsMetadata:
                 file_string+=ifile+';'
         sds_metadata['eStation2_input_files'] = file_string
 
-    def assign_scaling(self, scaling_factor, scaling_offset, nodata):
+    def assign_scaling(self, scaling_factor, scaling_offset, nodata, unit):
     #
     #   Assign scaling
         sds_metadata['eStation2_scaling_factor'] = str(scaling_factor)
         sds_metadata['eStation2_scaling_offset'] = str(scaling_offset)
         sds_metadata['eStation2_nodata'] = str(nodata)
+        sds_metadata['eStation2_unit'] = str(unit)
 
     def get_item(self, itemname):
 
