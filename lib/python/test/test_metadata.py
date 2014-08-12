cfac56d97f6ba4b5e9da7de1c6b5ca3fb54b8160
clerima <marco.clerici@jrc.ec.europa.eu>
2014-08-12 17:50:22 +0200

    Completely changed

--- lib/python/test/test_metadata.py
+++ lib/python/test/test_metadata.py
@@ -8,41 +8,80 @@ __author__ = 'clerima'
 
 import locals
 from osgeo import gdal
+from osgeo.gdalconst import *
 from lib.python.metadata import *
+import tempfile
+import shutil
 
-# input_dir = locals.es2globals['data_dir']+'FEWSNET_RFE/derived/10davg/'
-# file=input_dir+'0101_FEWSNET_RFE_10davg_FEWSNET_Africa_8km.tif'
-
-input_dir = locals.es2globals['data_dir']+'FEWSNET_RFE/tif/RFE/'
-file=input_dir+'20100101_FEWSNET_RFE_RFE_FEWSNET_Africa_8km.tif'
+input_dir = locals.es2globals['test_data_refs_dir']+'Metadata/'
+file=input_dir+'20140601_FEWSNET_RFE_RFE_FEWSNET_Africa_8km.tif'
 
 class TestMapSet(TestCase):
-    # def test_reading_meta_item(self):
-    #
-    #     fid = gdal.Open(file)
-    #     sds_meta = SdsMetadata()
-    #     sds_meta.read_from_ds(fid)
-    #     sds_meta.print_out()
 
-    def test_write_meta_into_domain(self):
+    def test_writing_reading_an_item(self):
 
-        item='new_item'
-        domain='my_own_domain'
-        value='new_value'
+        my_item='Test_Metadata_Item'
+        my_value='Test_Metadata_Value'
 
-        fid = gdal.Open(file)
-        #sds_meta = SdsMetadata()
-        fid.SetMetadataItem(item,value,domain)
+        # Create a tmp Tiff file
+        try:
+            tmpdir = tempfile.mkdtemp(prefix=__name__, suffix='_test_writing_an_item', dir=locals.es2globals['temp_dir'])
+        except IOError:
+            logger.error('Cannot create temporary dir ' + locals.es2globals['temp_dir'] + '. Exit')
+            return 1
 
-    def test_read_meta_from_domain(self):
+        filename = tmpdir+'/temp_target.tif'
 
-        my_domain='my_own_domain'
+        gtiff_driver = gdal.GetDriverByName('GTiff')
+        out_ds = gtiff_driver.Create(filename, 1, 1, 1, 1)
 
-        fid = gdal.Open(file)
-        # Get from default domain
-        list = fid.GetMetadata_List()
-        print ' Metadata from default domain'
-        print list
-        list = fid.GetMetadata_List(my_domain)
-        print ' Metadata from my_domain '
-        print list
+        out_ds.SetMetadataItem(my_item, my_value)
+
+        # Close the file and remove temp dir
+        out_ds = None
+
+        # Open the file for reading
+        in_ds = gdal.Open(filename, GA_ReadOnly)
+        read_value=in_ds.GetMetadataItem(my_item)
+
+        self.assertEqual(my_value, read_value)
+
+        # Remove the tmp dir
+        shutil.rmtree(tmpdir)
+
+    def test_reading_meta_item(self):
+
+        # Read from a reference file
+         fid = gdal.Open(file)
+         sds_meta = SdsMetadata()
+         sds_meta.read_from_ds(fid)
+         sds_meta.print_out()
+
+         value = sds_meta.get_item('eStation2_mapset')
+         self.assertEqual(value, 'FEWSNET_Africa_8km')
+
+         value = sds_meta.get_item('eStation2_nodata')
+         self.assertEqual(value, '-32768')
+
+         value = sds_meta.get_item('eStation2_version')
+         self.assertEqual(value, 'undefined')
+
+         value = sds_meta.get_item('eStation2_conversion')
+         self.assertEqual(value, 'Phys = DN * scaling_factor + scaling_offset')
+
+         value = sds_meta.get_item('eStation2_input_files')
+         self.assertEqual(value, '/data/Archives/FewsNET/a14061rb.zip;')
+
+         value = sds_meta.get_item('eStation2_subProduct')
+         self.assertEqual(value, 'rfe')
+
+         value = sds_meta.get_item('eStation2_product')
+         self.assertEqual(value, 'fewsnet_rfe')
+
+         value = sds_meta.get_item('eStation2_scaling_factor')
+         self.assertEqual(value, '1.0')
+
+         value = sds_meta.get_item('eStation2_unit')
+         self.assertEqual(value, None)
+
+
