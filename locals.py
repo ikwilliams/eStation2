644ebd737e6b9768f41c2f9027303c060d3a89e0
clerima <marco.clerici@jrc.ec.europa.eu>
2014-08-12 17:52:12 +0200

    Add the definition of the testing directories

--- locals.py
+++ locals.py
@@ -16,12 +16,19 @@ es2globals = {
     'dbPass': 'mesadmin',
     'dbName': 'estationdb',
     'schema': 'products',
-    'base_dir': '/srv/www/eStation2/',
-    'data_dir': '',
-    'ingest_dir': '',
+    'base_dir': '/srv/www/eStation2/',              # eStation2 installation dir
+    'data_dir': '',                                 # root dir for all eStation2 operational data
+    'ingest_dir': '',                               # 'pool' dir for all retrieved files (GET service)
     'static_data_path': '',
     'temp_dir': '/tmp/eStation2/'
 }
+
+# Add testing directories
+
+es2globals['test_base_dir']     =  es2globals['base_dir'] + 'TestFiles/'
+es2globals['test_data_in_dir']  =  es2globals['test_base_dir'] + 'Inputs/'
+es2globals['test_data_refs_dir'] =  es2globals['test_base_dir'] + 'RefsOutput/'
+es2globals['test_data_out']     =  es2globals['test_base_dir'] + 'Outputs/'
 
 try:
     from my_locals import *
