d33132b91eedebf01f11835c871f8e291f835775
clerima <marco.clerici@jrc.ec.europa.eu>
2014-08-20 18:27:59 +0200

    Tests added for new methods

--- lib/python/test/test_metadata.py
+++ lib/python/test/test_metadata.py
@@ -49,7 +49,56 @@ class TestMapSet(TestCase):
         # Remove the tmp dir
         shutil.rmtree(tmpdir)
 
-    def test_reading_meta_item(self):
+    def test_writing_meta_to_new_file(self):
+
+        sds_meta = SdsMetadata()
+
+        # Create a dummy output File
+        try:
+            tmpdir = tempfile.mkdtemp(prefix=__name__, suffix='_writing_meta_to_new_file', dir=locals.es2globals['temp_dir'])
+        except IOError:
+            logger.error('Cannot create temporary dir ' + locals.es2globals['temp_dir'] + '. Exit')
+            return 1
+
+        # Dummy output File
+        filename = tmpdir+'/temp_target.tif'
+        gtiff_driver = gdal.GetDriverByName('GTiff')
+        out_ds = gtiff_driver.Create(filename, 1, 1, 1, 1)
+
+        sds_meta.write_to_ds(out_ds)
+
+        # Close the file
+        out_ds = None
+        gtiff_driver = None
+
+        self.assertTrue(os.path.isfile(filename))
+
+    def test_writing_meta_to_existing_file(self):
+
+        sds_meta = SdsMetadata()
+
+        # Create a dummy output File
+        try:
+            tmpdir = tempfile.mkdtemp(prefix=__name__, suffix='_writing_meta_to_new_file', dir=locals.es2globals['temp_dir'])
+        except IOError:
+            logger.error('Cannot create temporary dir ' + locals.es2globals['temp_dir'] + '. Exit')
+            return 1
+
+        # Dummy output File
+        filename = tmpdir+'/temp_target.tif'
+        gtiff_driver = gdal.GetDriverByName('GTiff')
+        out_ds = gtiff_driver.Create(filename, 1, 1, 1, 1)
+
+        # Close the file
+        out_ds = None
+        gtiff_driver = None
+
+        sds_meta.write_to_file(filename)
+
+        self.assertTrue(os.path.isfile(filename))
+
+
+    def test_reading_meta_items_from_ds(self):
 
         # Read from a reference file
          fid = gdal.Open(file)
@@ -63,8 +112,8 @@ class TestMapSet(TestCase):
          value = sds_meta.get_item('eStation2_nodata')
          self.assertEqual(value, '-32768')
 
-         value = sds_meta.get_item('eStation2_version')
-         self.assertEqual(value, 'undefined')
+         value = sds_meta.get_item('eStation2_es2_version')
+         self.assertEqual(value, 'my_eStation2_sw_release')
 
          value = sds_meta.get_item('eStation2_conversion')
          self.assertEqual(value, 'Phys = DN * scaling_factor + scaling_offset')
@@ -84,4 +133,37 @@ class TestMapSet(TestCase):
          value = sds_meta.get_item('eStation2_unit')
          self.assertEqual(value, None)
 
+    def test_reading_meta_items_from_file(self):
+
+         sds_meta = SdsMetadata()
+        # Read from a reference file
+         sds_meta.read_from_file(file)
+         sds_meta.print_out()
+
+         value = sds_meta.get_item('eStation2_mapset')
+         self.assertEqual(value, 'FEWSNET_Africa_8km')
+
+         value = sds_meta.get_item('eStation2_nodata')
+         self.assertEqual(value, '-32768')
+
+         value = sds_meta.get_item('eStation2_es2_version')
+         self.assertEqual(value, 'my_eStation2_sw_release')
+
+         value = sds_meta.get_item('eStation2_conversion')
+         self.assertEqual(value, 'Phys = DN * scaling_factor + scaling_offset')
+
+         value = sds_meta.get_item('eStation2_input_files')
+         self.assertEqual(value, '/data/Archives/FewsNET/a14061rb.zip;')
+
+         value = sds_meta.get_item('eStation2_subProduct')
+         self.assertEqual(value, 'rfe')
+
+         value = sds_meta.get_item('eStation2_product')
+         self.assertEqual(value, 'fewsnet_rfe')
+
+         value = sds_meta.get_item('eStation2_scaling_factor')
+         self.assertEqual(value, '1.0')
+
+         value = sds_meta.get_item('eStation2_unit')
+         self.assertEqual(value, None)
 
