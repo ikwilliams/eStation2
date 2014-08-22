509021980cdd6932f4ba67149b5c330bef29d9e9
clerima <marco.clerici@jrc.ec.europa.eu>
2014-08-22 17:20:17 +0200

    Write one test (previously all test failed)

--- lib/python/test/test_mapSet.py
+++ lib/python/test/test_mapSet.py
@@ -2,43 +2,51 @@ from unittest import TestCase
 
 __author__ = 'tklooju'
 
+from lib.python.mapset import *
+
+#   TODO-M.C.: complete and re-activate all tests. Implement for the MapSet() class a 'isEqual' method.
 
 class TestMapSet(TestCase):
-    def test_assigndb(self):
-        self.fail()
 
-    def test_assign(self):
-        self.fail()
+    # def test_assigndb(self):
+    #     my_mapset = MapSet()
+    #
+    # def test_assign(self):
+    #     self.fail()
 
     def test_assign_default(self):
-        self.fail()
 
-    def test_assign_ecowas(self):
-        self.fail()
+        my_mapset = MapSet()
+        my_mapset.assign_default()
+        self.assertEqual(my_mapset.size_x, 9633)
+        self.assertEqual(my_mapset.size_y, 8177)
 
-    def test_assign_ioc_pml(self):
-        self.fail()
-
-    def test_assign_vgt4africa(self):
-        self.fail()
-
-    def test_assign_vgt4africa_500m(self):
-        self.fail()
-
-    def test_assign_msg_disk(self):
-        self.fail()
-
-    def test_assign_fewsnet_africa(self):
-        self.fail()
-
-    def test_assign_tamsat_africa(self):
-        self.fail()
-
-    def test_assign_modis_global(self):
-        self.fail()
-
-    def test_print_out(self):
-        self.fail()
-
-    def test_validate(self):
-        self.fail()
+    # def test_assign_ecowas(self):
+    #     self.fail()
+    #
+    # def test_assign_ioc_pml(self):
+    #     self.fail()
+    #
+    # def test_assign_vgt4africa(self):
+    #     self.fail()
+    #
+    # def test_assign_vgt4africa_500m(self):
+    #     self.fail()
+    #
+    # def test_assign_msg_disk(self):
+    #     self.fail()
+    #
+    # def test_assign_fewsnet_africa(self):
+    #     self.fail()
+    #
+    # def test_assign_tamsat_africa(self):
+    #     self.fail()
+    #
+    # def test_assign_modis_global(self):
+    #     self.fail()
+    #
+    # def test_print_out(self):
+    #     self.fail()
+    #
+    # def test_validate(self):
+    #     self.fail()
