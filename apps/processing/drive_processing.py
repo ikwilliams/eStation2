7f3b08c7c3a3676c6fedae1ea1fd327409f29d29
clerima <marco.clerici@jrc.ec.europa.eu>
2014-08-22 17:22:27 +0200

    Simple script to call the Processing chains  It replaces the previous test_processing.py

---
+++ apps/processing/drive_processing.py
@@ -0,0 +1,23 @@
+__author__ = "Marco Clerici"
+
+
+import time
+from apps.processing.processing_switches import *
+
+# General definitions/switches
+
+args = {'pipeline_run_level':pipeline_run_level, \
+        'pipeline_run_touch_only':pipeline_run_touch_only, \
+        'pipeline_printout_level':pipeline_printout_level, \
+        'pipeline_printout_graph_level':pipeline_printout_graph_level}
+
+from apps.processing.processing_fewsnet import *
+#from apps.processing.processing_ndvi import *
+
+start = time.clock()
+
+#   ---------------------------------------------------------------------
+#   Run the pipeline
+
+processing_fewsnet_rfe(**args)
+# processing_vgt_ndvi(**args)
