80feee62bed0d316e0143573ca2182b33c148d4b
clerima <marco.clerici@jrc.ec.europa.eu>
2014-09-16 11:53:48 +0200

    Remove my_locals from repo

--- my_locals.py
+++ my_locals.py
@@ -6,8 +6,8 @@
 import os, sys
 from locals import *
 
-oth_dir = '/srv/www/MarcoDev/eStation2/'
-my_dir = '/srv/www/JurDev/eStation2/'
+oth_dir = '/srv/www/JurDev/eStation2/'
+my_dir = '/srv/www/MarcoDev/eStation2/'
 
 list_syspath = sys.path
 
@@ -17,27 +17,14 @@ if my_dir not in list_syspath:
 if oth_dir in list_syspath:
     sys.path.remove(oth_dir)
 
-dir()
-
-es2globals = {
-    'host': 'localhost',
-    'port': '5432',
-    'dbUser': 'estation',
-    'dbPass': 'mesadmin',
-    'dbName': 'estationdb',
-    'schema': 'products',
-    'base_dir': '/srv/www/JurDev/eStation2/',
-    'data_dir': this_dir+'/TestFiles/',
-    'ingest_dir': this_dir+'/TestFiles/',
-    'static_data_path': '',
-    'temp_dir': '/tmp/eStation2/'
-}
-
-
-# Overwrite Ingest_dir (for vm19)
+# Overwrite definitions (for vm19)
+es2globals['base_dir']=my_dir
+es2globals['static_data_path']=''
+#es2globals['ingest_dir']=my_dir+'/TestFiles/'
 es2globals['ingest_dir'] = '/data/Archives/FewsNET/'
 
 # Overwrite data_fir (for vm19)
+#es2globals['data_dir']= my_dir+'/TestFiles/'
 es2globals['data_dir'] = '/data/processing/'
 
 # Add a test_data_dir (in/out/tmp)
