ff0203702835fdd40d6a1dd4b2d28c4e53dbd416
clerima <marco.clerici@jrc.ec.europa.eu>
2014-09-24 16:55:53 +0200

    Import connect_db for connection

--- database/crud.py
+++ database/crud.py
@@ -4,64 +4,26 @@ import sys
 # Import eStation lib modules
 from lib.python import es_logging as log
 from config import es_constants
+from database import connectdb
 
-
-from sqlalchemy import *
+import sqlalchemy
 from sqlalchemy.orm import *
 
 logger = log.my_logger(__name__)
 
 class CrudDB(object):
-    # @staticmethod
-    # def is_testing():
-    #     # Force connecting to sqlite db
-    #     # return True
-    #     if getattr(CrudDB, "_testing", None) is None:
-    #         setattr(CrudDB, "_testing", sys.argv[0].lower().endswith('nosetests'))
-    #     return CrudDB._testing
-    #Changes
-    # @staticmethod
-    # def get_db_url():
-    #     if CrudDB.is_testing():
-    #         if getattr(CrudDB, '_db_url', None) is None:
-    #             import sqlite3, os
-    #             # SQL Alchemy cound not execute full sql scripts
-    #             # so we use a regular file to import fixtures
-    #             #CrudDB._db_url = "sqlite://"
-    #             #con = sqlite3.connect(":memory:")
-    #             import tempfile
-    #             tf = tempfile.NamedTemporaryFile()
-    #             tmp_name = tf.name
-    #             tf.close()
-    #             CrudDB._db_url = "sqlite:///%s" % tmp_name
-    #             con = sqlite3.connect(tmp_name)
-    #             con.executescript(file(os.path.join(os.path.dirname(__file__), "fixtures", "sqlite.sql")).read())
-    #             con.close()
-    #             # Used in querydb
-    #             es_constants.dbglobals['schema_products'] = None
-    #         db_url = CrudDB._db_url
-    #     else:
-    #         db_url = "postgresql://%s:%s@%s/%s" % (es_constants.dbglobals['dbUser'],
-    #                                          es_constants.dbglobals['dbPass'],
-    #                                          es_constants.dbglobals['host'],
-    #                                          es_constants.dbglobals['dbName'])
-    #     return db_url
-    #
-    # @staticmethod
-    # def get_db_engine():
-    #     return create_engine(CrudDB.get_db_url())
 
     # Initialize the DB
-    def __init__(self, schema='products', echo=False):
-        self.schema = schema or es_constants.dbglobals['schema_products']
-        db = self.get_db_engine()
+    def __init__(self, schema='products'):
+        #self.schema = schema or es_constants.dbglobals['schema_products']
+        self.connect_db = connectdb.ConnectDB(schema=schema)
+        # db = connect_db.get_db_engine()
 
-        if self.is_testing():
-            self.schema = None
+        #if self.is_testing():
+            #self.schema = None
 
-        db.echo = echo
         self.table_map = {}
-        self.session = None
+        #self.session = None
 
         # new session
         #self.Session = Session()
@@ -69,7 +31,7 @@ class CrudDB(object):
         #db.execute("SET search_path TO products")
 
         #Initialize DB and create a hashmap of table name and associated ORM mapper class
-        metadata = MetaData(db, schema=self.schema)
+        metadata = sqlalchemy.MetaData(self.connect_db.db,schema=self.connect_db.schema)
         #retrieve database table information dynamically
         metadata.reflect()
         metadata.schema = None
@@ -77,7 +39,7 @@ class CrudDB(object):
             #create a class that inherits basetable class and maps the class to table
             table_class = type(str(table_name), (BaseTable,), {})
             try:
-                mapper(table_class, Table(table_name, metadata, autoload=True))
+                mapper(table_class, sqlalchemy.Table(table_name, metadata, autoload=True))
                 self.table_map[table_name] = table_class
             except:
                 #exceptiontype, exceptionvalue, exceptiontraceback = sys.exc_info()
@@ -86,8 +48,8 @@ class CrudDB(object):
                 logger.error("CrudDB: could not map table %s!" % table_name)
 
         #create a Session template that requires commit to be called explicit
-        self.session = sessionmaker(bind=db, autoflush=True)
-        metadata.schema = self.schema
+        #self.session = sessionmaker(bind=db, autoflush=True)
+        metadata.schema = self.connect_db.schema
 
     #create a record
     def create(self, table_name, record):
@@ -97,7 +59,7 @@ class CrudDB(object):
             #lookup the corresponding table class and create an instance
             table_instance = self.table_map[table_name]()
             table_instance.pack(record)
-            session = self.session()
+            session = self.connect_db.session()
             session.add(table_instance)
             session.commit()
             status = True
@@ -117,7 +79,7 @@ class CrudDB(object):
         records = []
         try:
             table_class = self.table_map[table_name]
-            session = self.session()
+            session = self.connect_db.session()
             query = session.query(table_class)
             resultset = query.filter_by(**keywords).all()
             for record in resultset:
@@ -141,7 +103,7 @@ class CrudDB(object):
             #lookup the corresponding table class and create an instance
             table_instance = self.table_map[table_name]()
             table_instance.pack(record)
-            session = self.session()
+            session = self.connect_db.session()
             session.merge(table_instance)
             session.commit()
             status = True
@@ -163,7 +125,7 @@ class CrudDB(object):
         try:
             #lookup the corresponding table class and create an instance
             table_class = self.table_map[table_name]
-            session = self.session()
+            session = self.connect_db.session()
             session.query(table_class).filter_by(**keywords).delete()
 
             session.commit()
