c3d80db0c69a1b6a0faa9e9c4ec7473014d6b6e1
clerima <marco.clerici@jrc.ec.europa.eu>
2014-10-02 18:40:11 +0200

    Remove obsolete test

--- apps/acquisition/test/test_get_eumetcast.py
+++ apps/acquisition/test/test_get_eumetcast.py
@@ -3,30 +3,30 @@ import unittest
 from apps.acquisition import get_eumetcast
 from database import querydb
 
-
-class TestGetEumetcast(unittest.TestCase):
-
-    #   ---------------------------------------------------------------------------
-    #   Test get_eumetcast_info()
-    #   ---------------------------------------------------------------------------
-    def TestGetEumetcastInfo(self):
-        db = querydb.db
-        data_acquisitions = querydb.get_data_acquisitions(echo=True, toJSON=False)
-
-        for row in data_acquisitions:
-            print row.data_source_id
-            # Retrieve datetime of latest acquired file and lastest datetime
-            # the acquisition was active of a specific eumetcast id
-            acq_dates = get_eumetcast.get_eumetcast_info(row.data_source_id)
-            if acq_dates:
-                for key in acq_dates.keys():
-                    print "key: %s , value: %s" % (key, acq_dates[key])
-                #print "time_latest_copy: "+acq_dates['time_latest_copy']
-                #print "time_latest_exec: "+acq_dates['time_latest_exec']
-            else:
-                print "Datasource " + row.data_source_id + " has not been activated!"
-            #logger.info('Datetime latest file: '+acq_dates)
-            #logger.info('Datetime last active: '+acq_dates)
-
-        self.assertEqual(1, 1)
+#
+# class TestGetEumetcast(unittest.TestCase):
+#
+#     #   ---------------------------------------------------------------------------
+#     #   Test get_eumetcast_info()
+#     #   ---------------------------------------------------------------------------
+#     def TestGetEumetcastInfo(self):
+#         db = querydb.db
+#         data_acquisitions = querydb.get_dataacquisitions(echo=True, toJSON=False)
+#
+#         for row in data_acquisitions:
+#             print row.data_source_id
+#             # Retrieve datetime of latest acquired file and lastest datetime
+#             # the acquisition was active of a specific eumetcast id
+#             acq_dates = get_eumetcast.get_eumetcast_info(row.data_source_id)
+#             if acq_dates:
+#                 for key in acq_dates.keys():
+#                     print "key: %s , value: %s" % (key, acq_dates[key])
+#                 #print "time_latest_copy: "+acq_dates['time_latest_copy']
+#                 #print "time_latest_exec: "+acq_dates['time_latest_exec']
+#             else:
+#                 print "Datasource " + row.data_source_id + " has not been activated!"
+#             #logger.info('Datetime latest file: '+acq_dates)
+#             #logger.info('Datetime last active: '+acq_dates)
+#
+#         self.assertEqual(1, 1)
 
