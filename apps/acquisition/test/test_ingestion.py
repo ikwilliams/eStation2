389cfb377a9c384ffa5cc80fa5afa085a3c19a2e
clerima <marco.clerici@jrc.ec.europa.eu>
2014-10-27 11:49:40 +0100

    Initial Test Case

--- apps/acquisition/test/test_ingestion.py
+++ apps/acquisition/test/test_ingestion.py
@@ -1,16 +1,22 @@
-_author__ = "Jurriaan van 't Klooster"
+_author__ = "Marco Clerici"
 
+
+import locals
 from apps.acquisition import ingestion
-#
-# import time
-#
-# start = time.clock()
-#
-# ingestion.drive_ingestion()
-#
-# elapsed = time.clock()-start
-# print elapsed
-#
-#
-# exit()
-#
+
+import unittest
+
+# Overwrite Dirs
+locals.es2globals['ingest_dir']=locals.es2globals['test_data_in_dir']
+locals.es2globals['data_dir']=locals.es2globals['test_data_out']
+
+class TestIngestion(unittest.TestCase):
+
+    def TestDriveAll(self):
+        ingestion.drive_ingestion()
+        self.assertEqual(1, 1)
+
+
+    def test_ingest_vgt_ndvi(self):
+        self.assertEqual(1, 1)
+
