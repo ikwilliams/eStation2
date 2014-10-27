4e722dd3064d37594ef3d39ff17071ff7c1beda5
clerima <marco.clerici@jrc.ec.europa.eu>
2014-10-27 11:56:11 +0100

    Minor Modifications

--- apps/acquisition/drive_ingestion.py
+++ apps/acquisition/drive_ingestion.py
@@ -1,11 +1,14 @@
 _author__ = "Jurriaan van 't Klooster"
 
+import locals
 from apps.acquisition import ingestion
 
 import time
 
 start = time.clock()
+locals.es2globals['ingest_dir']=locals.es2globals['test_data_in_dir']
 
+print locals.es2globals['data_dir']
 ingestion.drive_ingestion()
 
 elapsed = time.clock()-start
