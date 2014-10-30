2fc3094d0a28b77bb305e5779c606dc1f0a5ea20
Marco Beri <marcoberi@gmail.com>
2014-10-30 02:02:20 +0100

    Aggiunti file dimenticati

---
+++ apps/productmanagement/products.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+#
+#	purpose: Product functions
+#	author:  Marco Beri marcoberi@gmail.com
+#	date:	 27.10.2014
+
+from __future__ import absolute_import
+
+import datetime
+import os
+import glob
+
+import locals
+from lib.python import es_logging as log
+from lib.python import functions
+from database import querydb
+
+from .exceptions import (WrongFrequencyValue, WrongFrequencyUnit,
+                         WrongFrequencyType, WrongFrequencyDateFormat,
+                         NoProductFound, NoFrequencyFound,
+                         WrongDateType)
+from .datasets import Dataset
+
+logger = log.my_logger(__name__)
+
+class Product(object):
+    def __init__(self, product_code, version=None):
+        self.product_code = product_code
+        kwargs = {'productcode': self.product_code}
+        self.version=version
+        if not version is None:
+            kwargs['version'] = version
+        self._db_product = querydb.get_product_native(**kwargs)
+        if self._db_product is None:
+            raise NoProductFound(kwargs)
+        self._fullpath = os.path.join(locals.es2globals['data_dir'], product_code)
+
+    @property
+    def mapsets(self):
+        _mapsets = getattr(self, "_mapsets", None)
+        if _mapsets is None:
+            _mapsets = [os.path.basename(mapset) for mapset in self._get_full_mapsets()]
+            setattr(self, "_mapsets", _mapsets)
+        return _mapsets
+
+    def _get_full_mapsets(self):
+        return glob.glob(os.path.join(self._fullpath, "*"))
+
+    @property
+    def subproducts(self):
+        _subproducts = getattr(self, "_subproducts", None)
+        if _subproducts is None:
+            _subproducts = [subproduct for subproduct in 
+                    set(os.path.basename(subproduct) for subproduct in self._get_full_subproducts())]
+        return _subproducts
+
+    def _get_full_subproducts(self):
+        return tuple(subproduct for subproduct_type in functions.dict_subprod_type_2_dir.values()
+                                for subproduct in glob.glob(os.path.join(self._fullpath, subproduct_type)))
+
+    def get_dataset(self, mapset, sub_product_code):
+        return Dataset(self.product_code, sub_product_code=sub_product_code, mapset=mapset, version=self.version)
---
+++ apps/productmanagement/test/test_products.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+#
+#	purpose: Test products functions
+#	author:  Marco Beri marcoberi@gmail.com
+#	date:	 27.10.2014
+#
+
+from __future__ import absolute_import
+
+import unittest
+import os
+import datetime
+import sys
+from ..helpers import INTERVAL_TYPE
+from ..products import Product
+from ..datasets import Dataset
+from ..exceptions import (WrongDateType, NoProductFound)
+
+import locals
+from lib.python import functions
+from database import querydb
+import json
+
+class TestProducts(unittest.TestCase):
+    def setUp(self):
+        self.kwargs = {'product_code':"fewsnet_rfe"}
+        self.mapsets = ('FEWSNET_Africa_8km', 'WGS84_Africa_1km')
+        self.subproducts = ('10DAVG', 'rfe')
+        self.files_mapsets = [os.path.join(locals.es2globals['data_dir'],
+            self.kwargs['product_code'], mapset) for mapset in self.mapsets]
+        self.files_subproducts = [os.path.join(file_mapset, subproduct_type, subproduct)
+                for file_mapset in self.files_mapsets
+                for subproduct_type in functions.dict_subprod_type_2_dir.values()
+                for subproduct in self.subproducts]
+
+    def test_class(self):
+        self.assertIsInstance(Product(**self.kwargs), Product)
+
+    def test_class_no_product(self):
+        kwargs = {'product_code':"---prod---"}
+        self.assertRaisesRegexp(NoProductFound, "(?i).*found.*product.*", Product, **kwargs)
+
+    def test_class_mapsets(self):
+        product = Product(**self.kwargs)
+        product._get_full_mapsets = lambda: self.files_mapsets
+        self.assertEqual(len(product.mapsets), len(self.mapsets))
+        self.assertEqual(set(product.mapsets), set(self.mapsets))
+
+    def test_class_subproduct(self):
+        product = Product(**self.kwargs)
+        product._get_full_subproducts = lambda: self.files_subproducts
+        self.assertEqual(len(product.subproducts), len(self.subproducts))
+        self.assertEqual(set(product.subproducts), set(self.subproducts))
+
+    def test_class_dataset(self):
+        product = Product(**self.kwargs)
+        product._get_full_subproducts = lambda: self.files_subproducts
+        self.assertIsInstance(product.get_dataset(sub_product_code=product.subproducts[-1], mapset=self.mapsets[0]),
+            Dataset)
