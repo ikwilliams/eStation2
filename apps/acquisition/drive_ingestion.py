93433bc5c706c02982b62d48888df8f06fa6606e
clerima <marco.clerici@jrc.ec.europa.eu>
2014-11-17 11:33:22 +0100

    Adapt the routine to start/stop ingestion as a Service

--- apps/acquisition/drive_ingestion.py
+++ apps/acquisition/drive_ingestion.py
@@ -1,17 +1,35 @@
 _author__ = "Jurriaan van 't Klooster"
 
 import locals
+import os, time
+from config import es_constants
 from apps.acquisition import ingestion
+from apps.acquisition import acquisition
+from lib.python import es_logging as log
+logger = log.my_logger(__name__)
 
-import time
+# Manual Switch for START/STOP
+do_start = False
+dry_run  = True
 
-start = time.clock()
-locals.es2globals['ingest_dir']=locals.es2globals['test_data_in_dir']
+pid_file = es_constants.ingest_pid_filename
+daemon = acquisition.IngestDaemon(pid_file, dry_run=dry_run)
 
-print locals.es2globals['data_dir']
-ingestion.drive_ingestion()
+# locals.es2globals['ingest_dir']=locals.es2globals['test_data_in_dir']
 
-elapsed = time.clock()-start
-print elapsed
+
+if do_start:
+    if daemon.status():
+        logger.info('Ingest process is running: Exit')
+    else:
+        logger.info('Ingest process is NOT running: Start it.')
+        daemon.start()
+else:
+    if not daemon.status():
+        logger.info('Ingest process is NOT running: Exit')
+    else:
+        logger.info('Ingest process is running: Stop it.')
+        daemon.stop()
+
 
 
