1fbd505cd2992c1177d0e8fbe6079d7ceae31667
clerima <marco.clerici@jrc.ec.europa.eu>
2014-11-19 15:02:06 +0100

    Update the 'drive' routine to run bot as a service (default) and as a single call

--- apps/acquisition/drive_get_eumetcast.py
+++ apps/acquisition/drive_get_eumetcast.py
@@ -11,23 +11,33 @@ logger = log.my_logger(__name__)
 # Manual Switch for START/STOP
 do_start = False
 dry_run = True
+service  = False
 
-pid_file = es_constants.get_eumetcast_pid_filename
-daemon = acquisition.GetEumetcastDaemon(pid_file, dry_run=dry_run)
+if service:
+    # Make sure the pid dir exists
+    if not os.path.isdir(es_constants.pid_file_dir):
+        try:
+            os.makedirs(es_constants.pid_file_dir)
+        except os.error:
+            logger.error("Cannot create pid directory")
 
+    # Define pid file and create daemon
+    pid_file = es_constants.get_eumetcast_pid_filename
+    daemon = acquisition.GetEumetcastDaemon(pid_file, dry_run=dry_run)
 
-if do_start:
-    if daemon.status():
-        logger.info('GetEumetcast process is running: Exit')
+    if do_start:
+        if daemon.status():
+            logger.info('GetEumetcast process is running: Exit')
+        else:
+            logger.info('GetEumetcast process is NOT running: Start it.')
+            daemon.start()
     else:
-        logger.info('GetEumetcast process is NOT running: Start it.')
-        daemon.start()
+        if not daemon.status():
+            logger.info('GetEumetcast process is NOT running: Exit')
+        else:
+            logger.info('GetEumetcast process is running: Stop it.')
+            daemon.stop()
 else:
-    if not daemon.status():
-        logger.info('GetEumetcast process is NOT running: Exit')
-    else:
-        logger.info('GetEumetcast process is running: Stop it.')
-        daemon.stop()
-
+    get_eumetcast.drive_eumetcast(dry_run=dry_run)
 
 
--- apps/acquisition/drive_get_internet.py
+++ apps/acquisition/drive_get_internet.py
@@ -8,28 +8,35 @@ from apps.acquisition import acquisition
 from lib.python import es_logging as log
 logger = log.my_logger(__name__)
 
-
-#get_internet.drive_get_internet()
-
 # Manual Switch for START/STOP
 do_start = False
 dry_run  = True
+service  = False
 
-pid_file = es_constants.get_internet_pid_filename
-daemon = acquisition.GetInternetDaemon(pid_file, dry_run=dry_run)
+if service:
+    # Make sure the pid dir exists
+    if not os.path.isdir(es_constants.pid_file_dir):
+        try:
+            os.makedirs(es_constants.pid_file_dir)
+        except os.error:
+            logger.error("Cannot create pid directory")
 
+    # Define pid file and create daemon
+    pid_file = es_constants.get_internet_pid_filename
+    daemon = acquisition.GetInternetDaemon(pid_file, dry_run=dry_run)
 
-if do_start:
-    if daemon.status():
-        logger.info('GetInternet process is running: Exit')
+    if do_start:
+        if daemon.status():
+            logger.info('GetInternet process is running: Exit')
+        else:
+            logger.info('GetInternet process is NOT running: Start it.')
+            daemon.start()
     else:
-        logger.info('GetInternet process is NOT running: Start it.')
-        daemon.start()
+        if not daemon.status():
+            logger.info('GetInternet process is NOT running: Exit')
+        else:
+            logger.info('GetInternet process is running: Stop it.')
+            daemon.stop()
 else:
-    if not daemon.status():
-        logger.info('GetInternet process is NOT running: Exit')
-    else:
-        logger.info('GetInternet process is running: Stop it.')
-        daemon.stop()
-
+    get_internet.drive_get_internet(dry_run=dry_run)
 
