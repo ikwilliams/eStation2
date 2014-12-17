f0a94256b104a3f2dbaacf4c26585be4a9f98a0c
clerima <marco.clerici@jrc.ec.europa.eu>
2014-12-17 19:13:15 +0100

    Infinite loop for Processing

--- apps/processing/processing.py
+++ apps/processing/processing.py
@@ -8,7 +8,7 @@ _author__ = "Marco Clerici"
 #
 
 # source eStation2 base definitions
-import locals
+import locals, os, sys
 
 # import standard modules
 import time
@@ -24,8 +24,7 @@ data_dir= locals.es2globals['data_dir']
 
 #   Main module in processing, driving a specific pipeline
 
-from apps.processing import processing_fewsnet
-from apps.processing import processing_ndvi
+from apps.processing import processing_std_precip
 from lib.python.daemon import DaemonDryRunnable
 
 def loop_processing(dry_run=False):
@@ -37,23 +36,91 @@ def loop_processing(dry_run=False):
 #    Arguments: dry_run -> if > 0, it triggers pipeline_printout() rather than pipeline_run()
 #                       -> if < 0, it triggers pipeline_printout_graph() rather than pipeline_run()
 
+    # TODO-M.C.: clean dir with locks !!
 
     logger.info("Entering routine %s" % 'loop_processing')
     echo_query = False
-
-    while True:
+    functions.check_output_dir(es_constants.processing_tasks_dir)
+    while True :
 
         # Get all active processing chains from the database.
-        active_processing_chains = querydb.get_processing_chains(allrecs=True, echo=echo_query)
+        # active_processing_chains = querydb.get_processing_chains(allrecs=True, echo=echo_query)
+        #
+        # for active_chain in active_processing_chains:
+        #
+        #     logger.info("Processing active for product: [%s] subproduct N. %s" % (active_processing_chain[0],
+        #                                                                           active_processing_chain[2]))
+        #     productcode = active_product_ingest[0]
+        #     productversion = active_product_ingest[1]
 
-        for active_chain in active_processing_chains:
+        logger.debug("Entering infinite loop")
 
-            logger.info("Processing active for product: [%s] subproduct N. %s" % (active_processing_chain[0],
-                                                                                  active_processing_chain[2]))
-            productcode = active_product_ingest[0]
-            productversion = active_product_ingest[1]
+        # First processing
+        # The following id comes either from the DB id, or a combination of fields
+        processing_unique_id='0000001'
+        processing_unique_lock=es_constants.processing_tasks_dir+processing_unique_id
 
+        args = {'pipeline_run_level':0, \
+                'pipeline_run_touch_only':0, \
+                'pipeline_printout_level':1, \
+                'pipeline_printout_graph_level':0, \
+                'starting_sprod': 'rfe', \
+                'prod':"fewsnet_rfe", \
+                'mapset':'FEWSNET_Africa_8km',\
+                'version':'undefined'}
 
+        if not os.path.isfile(processing_unique_lock):
+            logger.debug("Launching processing for ID: %s" % processing_unique_id)
+            open(processing_unique_lock,'a').close()
+            # fork and call the std_precip 'generic' processing
+            pid = os.fork()
+            if pid == 0:
+                # Call to the processing pipeline
+                processing_std_precip.processing_std_precip(**args)
+                # Simulate longer processing (TEMP)
+                time.sleep(10)
+                os.remove(processing_unique_lock)
+                sys.exit(0)
+            else:
+                # Qui sono il padre
+                pass
+                #os.wait()
+        else:
+            logger.debug("Processing already running for ID: %s " % processing_unique_id)
+
+        # Second processing (to be removed - do a loop)
+        processing_unique_id='0000002'
+        processing_unique_lock=es_constants.processing_tasks_dir+processing_unique_id
+
+        args = {'pipeline_run_level':0, \
+                'pipeline_run_touch_only':0, \
+                'pipeline_printout_level':1, \
+                'pipeline_printout_graph_level':0, \
+                'starting_sprod': 'rfe', \
+                'prod':"fewsnet_rfe", \
+                'mapset':'FEWSNET_AFRICA_8km',\
+                'version':'undefined'}
+
+        if not os.path.isfile(processing_unique_lock):
+            logger.debug("Launching processing for ID: %s" % processing_unique_id)
+            open(processing_unique_lock,'a').close()
+            # fork and call the std_precip 'generic' processing
+            pid = os.fork()
+            if pid == 0:
+                # Call to the processing pipeline
+                processing_std_precip.processing_std_precip(**args)
+                # Simulate longer processing (TEMP)
+                time.sleep(7)
+                os.remove(processing_unique_lock)
+                sys.exit(0)
+            else:
+                # Qui sono il padre
+                pass
+                #os.wait()
+        else:
+            logger.debug("Processing already running for ID: %s " % processing_unique_id)
+
+        time.sleep(1)
 
 class ProcessingDaemon(DaemonDryRunnable):
     def run(self):
