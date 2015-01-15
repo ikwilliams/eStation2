01443d93e1f7dbf49b0bd4053df5a126863a87a3
e-Station Thematic User <analyst@ubuntu.(none)>
2015-01-15 14:46:02 +0000

    Additional scripts to check/start/stop service

---
+++ apps/acquisition/service_ingestion.py
@@ -0,0 +1,25 @@
+_author__ = "Marco Clerici"
+
+import locals
+import sys
+import os, time
+from config import es_constants
+from apps.acquisition import get_internet
+from apps.acquisition import acquisition
+from lib.python import es_logging as log
+logger = log.my_logger(__name__)
+
+command=str(sys.argv[1])
+# Define pid file and create daemon
+pid_file = es_constants.ingest_pid_filename
+daemon = acquisition.IngestDaemon(pid_file, dry_run=0)
+
+if command=="status":
+        status=daemon.status()
+        print("Current status of the Service: %s" % status)
+    
+if command=="start":
+        daemon.start()
+        
+if command=="stop":
+        daemon.stop()
