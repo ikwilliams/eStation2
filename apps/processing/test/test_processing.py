0e6cfb9fa0afb3403d02ba323050261896e5aeba
e-Station Thematic User <analyst@ubuntu.(none)>
2015-01-25 17:46:54 +0000

    Use test_processing to call/test product-dedicated routine

--- apps/processing/test/test_processing.py
+++ apps/processing/test/test_processing.py
@@ -4,28 +4,18 @@ __author__ = "Marco Clerici"
 import time
 
 # General definitions/switches
-starting_sprod='rfe'
-prod="fewsnet_rfe"
-mapset='FEWSNET_Africa_8km'
-version='undefined'
-
-# General definitions/switches
 args = {'pipeline_run_level':1, \
         'pipeline_run_touch_only':0, \
         'pipeline_printout_level':0, \
         'pipeline_printout_graph_level':0}
 
-#from apps.processing.processing_std_precip import *
-#from apps.processing.processing_ndvi import *
+from apps.processing.processing_ndvi import *
 from apps.processing.processing_fewsnet import *
 
 start = time.clock()
 
-#list_prods = processing_std_precip(**args)
-#list_sprods = get_subprods_std_precip()
-
 #   ---------------------------------------------------------------------
 #   Run the pipeline
 
-processing_fewsnet_rfe(**args)
-#processing_ndvi(**args)
+#processing_fewsnet_rfe(**args)
+processing_ndvi(**args)
