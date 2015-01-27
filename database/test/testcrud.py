db4b4938bf46b46515bd8a4b02c08bbcb3697370
Jurvtk <jurriaan.van-t-Klooster@ext.jrc.ec.europa.eu>
2015-01-27 13:00:19 +0100

    GUI Build error solved

--- database/test/testcrud.py
+++ database/test/testcrud.py
@@ -5,6 +5,7 @@ __author__ = "Jurriaan van 't Klooster"
 import unittest
 from config import es_constants
 from database import crud
+from database import querydb
 
 crud_db = crud.CrudDB(schema=es_constants.es2globals['schema_products'])
 
@@ -12,8 +13,6 @@ crud_db = crud.CrudDB(schema=es_constants.es2globals['schema_products'])
 class TestCrud(unittest.TestCase):
 
     def test_crud(self):
-        #schema='products.'
-        schema = ''
         records = len(crud_db.read('date_format'))
         self.assertTrue(records > 0)
 
@@ -31,9 +30,14 @@ class TestCrud(unittest.TestCase):
 
         self.assertEquals(len(crud_db.read('date_format')), records)
 
-        productinfo = {'productcode': 'vgt_fapar', 'activated': False, 'version': 'V1.3', 'product_type': 'Native', 'subproductcode': 'vgt_fapar_native'}
+        productinfo = {'productcode': 'vgt_fapar', 'subproductcode': 'vgt_fapar_native', 'version': 'V1.3', 'defined_by': 'JRC', 'activated': False}
         crud_db.update('product', productinfo)
-        self.assertEquals(len(crud_db.read(schema + 'product')), records)
 
+        produpdated = querydb.get_product_native(productcode='vgt_fapar', version='V1.3', allrecs=False, echo=False)
+        self.assertEquals(produpdated.activated, False)
 
+        productinfo = {'productcode': 'vgt_fapar', 'subproductcode': 'vgt_fapar_native', 'version': 'V1.3', 'defined_by': 'JRC', 'activated': True}
+        crud_db.update('product', productinfo)
 
+        produpdated = querydb.get_product_native(productcode='vgt_fapar', version='V1.3', allrecs=False, echo=False)
+        self.assertEquals(produpdated.activated, True)
