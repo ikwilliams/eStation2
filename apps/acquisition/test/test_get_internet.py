c0aa5050e709e5ed6dca3ec117661ca30adb1521
clerima <marco.clerici@jrc.ec.europa.eu>
2015-02-20 16:07:29 +0100

    Implement missing cases (%dkm, +8j)

--- apps/acquisition/test/test_get_internet.py
+++ apps/acquisition/test/test_get_internet.py
@@ -12,22 +12,35 @@ class TestGetInternet(unittest.TestCase):
         info = get_eumetcast_info(eum_id)
 
     #   ---------------------------------------------------------------------------
-    #   Test iteration on remote ftp (e.g. MODIS)
+    #   Get contents of a remote MODIS BA  (id: UMD:MCD45A1:TIF:51)
     #   ---------------------------------------------------------------------------
-    def TestRemoteFtp_MODIS_BA_monthly(self):
-
-        # Retrieve a list of MODIS burndate file .. check only one present
-        remote_url='ftp://ba1.geog.umd.edu'
+    def TestRemoteFtp_MCD45A1_TIF(self):
+        remote_url='ftp://ba1.geog.umd.edu/Collection51/TIFF/'
         usr_pwd='user:burnt_data'
-        full_regex   ='Collection51/TIFF/Win11/201./MCD45monthly.A20.*burndate.tif.gz'
-        file_to_check='Collection51/TIFF/Win11/2011/MCD45monthly.A2011001.Win11.051.burndate.tif.gz'
+        #full_regex   ='Win.*/20../MCD45monthly.*.burndate.tif.gz'
+        full_regex   ='Win11/2011/MCD45monthly.*.burndate.tif.gz'
+        file_to_check='Win11/2011/MCD45monthly.A2011001.Win11.051.burndate.tif.gz'
+
 
         list = get_list_matching_files_dir_ftp(remote_url, usr_pwd, full_regex)
-
         self.assertTrue(file_to_check in list)
 
     #   ---------------------------------------------------------------------------
-    #   Test iteration on remote ftp (e.g. NASA_FIRMS)
+    #   Get contents of a remote MODIS BA  (id: UMD:MCD45A1:HDF:51)
+    #   ---------------------------------------------------------------------------
+    def TestRemoteFtp_MCD45A1_HDF(self):
+        remote_url='ftp://ba1.geog.umd.edu/Collection51/HDF/'
+        usr_pwd='user:burnt_data'
+        #full_regex   ='20../.../MCD45A1.A.*.hdf'
+        full_regex   ='2011/.../MCD45A1.A.*.hdf'
+        file_to_check='2011/001/MCD45A1.A2011001.h05v10.051.2013067232210.hdf'
+
+        list = get_list_matching_files_dir_ftp(remote_url, usr_pwd, full_regex)
+        self.assertTrue(file_to_check in list)
+
+
+    #   ---------------------------------------------------------------------------
+    #   Test remote ftp NASA_FIRMS (id: USGS:FIRMS)
     #   ---------------------------------------------------------------------------
     def TestRemoteFtp_FIRMS_NASA(self):
 
@@ -42,34 +55,75 @@ class TestGetInternet(unittest.TestCase):
         self.assertTrue(file_to_check in list)
 
     #   ---------------------------------------------------------------------------
-    #   Test iteration on remote ftp (e.g. ARC2)
+    #   Test iteration on ftp CHIRP (id: UCSB:CHIRP:DEKAD)
     #   ---------------------------------------------------------------------------
-    def TestRemoteFtp_ARC2(self):
+    def TestRemoteFtp_CHIRP(self):
+        # Retrieve a list of CHIRP
+        remote_url='ftp://chg-ftpout.geog.ucsb.edu/pub/org/chg/products/CHIRP/pentads/'
+        usr_pwd='anonymous:anonymous'
+        full_regex   ='CHIRP.2014.12.[1-3].tif'
+        file_to_check='CHIRP.2014.12.1.tif'
+
+        list = get_list_matching_files_dir_ftp(remote_url, usr_pwd, full_regex)
+        self.assertTrue(file_to_check in list)
+
+    #   ---------------------------------------------------------------------------
+    #   Test iteration on ftp CHIRPS (id to )
+    #   ---------------------------------------------------------------------------
+    def TestRemoteFtp_CHIRPS(self):
+        # Retrieve a list of CHIRP
+        remote_url='ftp://chg-ftpout.geog.ucsb.edu/pub/org/chg/products/CHIRPS-latest/prelim/global_pentad/tifs/'
+        usr_pwd='anonymous:anonymous'
+        full_regex   ='chirps-v1.8.*.tif'
+        file_to_check='chirps-v1.8.2014.09.4.tif'
+
+        list = get_list_matching_files_dir_ftp(remote_url, usr_pwd, full_regex)
+        self.assertTrue(file_to_check in list)
+
+     #   ---------------------------------------------------------------------------
+    #   Test remote ftp NOAA GSOD (id: NOAA:GSOD)
+    #   ---------------------------------------------------------------------------
+    def TestRemoteFtp_NOAA_GSOD(self):
 
         # Retrieve a list of MODIS burndate file .. check only one present
-        remote_url='ftp://ftp.cpc.ncep.noaa.gov/fews/fewsdata/africa/arc2/geotiff/'
-        usr_pwd='anonymous:@'
-        full_regex   ='africa_arc.2015.....tif.zip'
-        file_to_check='africa_arc.20150121.tif.zip'
+        remote_url='ftp://ftp.ncdc.noaa.gov/pub/data/gsod/'
+        usr_pwd='anonymous:'
+        full_regex   ='2011/997...-99999-2011.op.gz'
+        file_to_check='2011/997286-99999-2011.op.gz'
 
         list = get_list_matching_files_dir_ftp(remote_url, usr_pwd, full_regex)
 
         self.assertTrue(file_to_check in list)
-    
+
     #   ---------------------------------------------------------------------------
-    #   Test iteration on remote ftp (e.g. CAMS_OPI)
+    #   Test iteration on remote ftp (e.g. ARC2)
     #   ---------------------------------------------------------------------------
-    def TestRemoteFtp_CAMS_OPI(self):
-
-        # Retrieve a list of CQMS-OPI .. check only one present
-        remote_url='ftp://ftp.cpc.ncep.noaa.gov/precip/data-req/cams_opi_v0208/'
-        usr_pwd='anonymous:@'
-        full_regex   ='africa_arc.2015.....tif.zip'
-        file_to_check='africa_arc.20150121.tif.zip'
-
-        list = get_list_matching_files_dir_ftp(remote_url, usr_pwd, full_regex)
-
-        self.assertTrue(file_to_check in list)
+    # def TestRemoteFtp_ARC2(self):
+    #
+    #     # Retrieve a list of MODIS burndate file .. check only one present
+    #     remote_url='ftp://ftp.cpc.ncep.noaa.gov/fews/fewsdata/africa/arc2/geotiff/'
+    #     usr_pwd='anonymous:@'
+    #     full_regex   ='africa_arc.2015.....tif.zip'
+    #     file_to_check='africa_arc.20150121.tif.zip'
+    #
+    #     list = get_list_matching_files_dir_ftp(remote_url, usr_pwd, full_regex)
+    #
+    #     self.assertTrue(file_to_check in list)
+    #
+    # #   ---------------------------------------------------------------------------
+    # #   Test iteration on remote ftp (e.g. CAMS_OPI)
+    # #   ---------------------------------------------------------------------------
+    # def TestRemoteFtp_CAMS_OPI(self):
+    #
+    #     # Retrieve a list of CQMS-OPI .. check only one present
+    #     remote_url='ftp://ftp.cpc.ncep.noaa.gov/precip/data-req/cams_opi_v0208/'
+    #     usr_pwd='anonymous:@'
+    #     full_regex   ='africa_arc.2015.....tif.zip'
+    #     file_to_check='africa_arc.20150121.tif.zip'
+    #
+    #     list = get_list_matching_files_dir_ftp(remote_url, usr_pwd, full_regex)
+    #
+    #     self.assertTrue(file_to_check in list)
 
 
     #   ---------------------------------------------------------------------------
@@ -88,77 +142,76 @@ class TestGetInternet(unittest.TestCase):
     #     self.assertTrue(file_to_check in list)
 
     #   ---------------------------------------------------------------------------
-    #   Test iteration on remote ftp (CHIRP/CHIRPS)
+    #   Test download of files from GSFC oceandata http site (id:GSFC:OCEAN:MODIS:SST:1D)
     #   ---------------------------------------------------------------------------
-    def TestRemoteFtp_CHIRP(self):
-        # Retrieve a list of CHIRP
-        remote_url='ftp://chg-ftpout.geog.ucsb.edu/pub/org/chg/products/CHIRPS-latest/africa_dekad/tifs/'
+    def TestRemoteHttp_MODIS_SST_1DAY(self):
+
+        #remote_url='http://oceandata.sci.gsfc.nasa.gov/'
+        from_date= datetime.date(2015,1,1)
+        to_date= datetime.date(2015,2,1)
+        template='%Y/%j/A%Y%j.L3m_DAY_SST_4.bz2'
         usr_pwd='anonymous:anonymous'
-        full_regex   ='chirps-v1.8.2014.12.[1-3].tif'
-        file_to_check='chirps-v1.8.2014.12.1.tif'
-        
-        list = get_list_matching_files_dir_ftp(remote_url, usr_pwd, full_regex)
-        self.assertTrue(file_to_check in list)
+        frequency='day'
+
+        files_list = build_list_matching_for_http(template, from_date, to_date, frequency)
+        file_to_check='2015/001/A2015001.L3m_DAY_SST_4.bz2'
+
+        print files_list
+        self.assertTrue(file_to_check in files_list)
 
     #   ---------------------------------------------------------------------------
-    #   Get contents of a directory (FTP)  -> MCD45A1_TIF_C51
-    #   A list of the dir is returned (as form ls -l)
+    #   Get a file from HTTP (list_from_template=TRUE, No User/psw) -> FEWSNET
     #   ---------------------------------------------------------------------------
-    def TestRemoteFtp_MCD45A1(self):
-        filename='mcd45a1.txt'
-        remote_url='ftp://ba1.geog.umd.edu/Collection5/'
-        UserPwd='user:burnt_data'
-        filepath = get_dir_contents_from_url(remote_url, target_file=filename, userpwd=UserPwd)
-        logger.info('File downloaded to: '+filepath)
-        self.assertEqual(1,1)
+
+    def TestRemoteHttp_FEWSNET(self):
+        #template='a%y%m1rb.zip'           # it works forcing 1 as dekad of month
+        template='a%y%m%{dkm}rb.zip'       # introduce non-standard placeholder
+        file_to_check='a14081rb.zip'
+        from_date = datetime.date(2014, 1, 1)
+        to_date = datetime.date(2014, 12, 31)
+        frequency = 'dekad'
+        files_list = build_list_matching_for_http(template, from_date, to_date, frequency)
+        print files_list
+
+        self.assertTrue(file_to_check in files_list)
 
     #   ---------------------------------------------------------------------------
     #   Get contents of a directory (HTTP)  -> MODIS_SST_8D
     #   An html document is returned
     #   ---------------------------------------------------------------------------
     def TestRemoteHttp_MODIS_SST_8D(self):
-        filename='A20100012010008.L3m_8D_SST_4.bz2'
-        remote_url='http://oceandata.sci.gsfc.nasa.gov/'
-        UserPwd='anonymous:anonymous'
-        filepath = get_file_from_url(remote_url, target_file=filename, userpwd=UserPwd)
-        logger.info('File downloaded to: '+filepath)
-        self.assertEqual(1,1)
 
-
-    #   ---------------------------------------------------------------------------
-    #   Get a file from HTTP (list_from_template=TRUE, No User/psw) -> FEWSNET
-    #   ---------------------------------------------------------------------------
-
-    def TestHttpDir_FEWSNET(self):
-        #filename=''
-        remote_url='http://earlywarning.usgs.gov/ftp2/raster/rf/a/2014/'
-        #UserPwd='anonymous:anonymous'
-        filepath = get_dir_contents_from_url(remote_url)
-        print(filepath)
-
-        #logger.info('File downloaded to: '+filepath)
-        #self.assertEqual(1,1)
-
-    def TestRemoteHttp_FEWSNET(self):
-        filename='a14081rb.zip'
-        remote_url='http://earlywarning.usgs.gov/ftp2/raster/rf/a/2014/'
-        # filepath = get_file_from_url(remote_url+filename, target_file=filename)
-        filepath = get_file_from_url(remote_url, target_file=filename)
-        print(filepath)
-        logger.info('File downloaded to: '+filepath)
-        self.assertEqual(1,1)
-        
-    #   ---------------------------------------------------------------------------
-    #   Test iteration on remote ftp (CHIRPS)
-    #   ---------------------------------------------------------------------------
-    def TestRemoteFtp_CHIRPS(self):
-        # Retrieve a list of CHIRP
-        remote_url='ftp://chg-ftpout.geog.ucsb.edu/pub/org/chg/products/CHIRPS-latest/prelim/global_pentad/tifs/'
+        #remote_url='http://oceandata.sci.gsfc.nasa.gov/'
+        from_date= datetime.date(2015,1,1)
+        to_date= datetime.date(2015,2,1)
+        template='A%Y%j%{Yj+8}.L3m_8D_SST_4.bz2'
         usr_pwd='anonymous:anonymous'
-        full_regex   ='chirps-v1.8.*.tif'
-        file_to_check='chirps-v1.8.2014.09.4.tif'
-        
-        list = get_list_matching_files_dir_ftp(remote_url, usr_pwd, full_regex)
-        self.assertTrue(file_to_check in list)
+        frequency='day'
 
- 
+        files_list = build_list_matching_for_http(template, from_date, to_date, frequency)
+        print files_list
+        self.assertEqual(1,1)
+
+    #   ---------------------------------------------------------------------------
+    #   Test download of 8DAY data from GSFC oceandata http site (id:GSFC:OCEAN:MODIS:SST:8D)
+    #   ---------------------------------------------------------------------------
+    def TestRemoteHttp_MODIS_SST_8DAY(self):
+
+        remote_url='http://oceandata.sci.gsfc.nasa.gov/MODISA/Mapped/8Day/4km/SST/2010/A2010193*.bz2'
+        usr_pwd='anonymous:anonymous'
+        c=pycurl.Curl()
+        import StringIO
+        import cStringIO
+        buffer = StringIO.StringIO()
+
+        c.setopt(c.URL, remote_url)
+        c.setopt(c.WRITEFUNCTION, buffer.write)
+        c.perform()
+        html = buffer.getvalue()
+
+        file_to_check='2015/001/A2015001.L3m_DAY_SST_4.bz2'
+        #results = re.search(r'(type="hidden" name="([0-9a-f]{32})")', html).group(2)
+
+
+        print results
+        self.assertTrue(file_to_check in results)
