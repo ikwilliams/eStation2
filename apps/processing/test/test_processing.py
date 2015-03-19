2b1873ba422cdd9afc526095c4894f9edc17044a
Mesa User <mesauser@mesa-proc.(none)>
2015-03-19 08:59:56 +0100

    Minor mods

--- apps/processing/test/test_processing.py
+++ apps/processing/test/test_processing.py
@@ -3,12 +3,14 @@ __author__ = "Marco Clerici"
 # General definitions/switches
 args = {'pipeline_run_level':0, \
         'pipeline_run_touch_only':0, \
-        'pipeline_printout_level':6, \
+        'pipeline_printout_level':1, \
         'pipeline_printout_graph_level': 0}
 
-from apps.processing.processing_modis_sst import *
+#from apps.processing.processing_ndvi import *
+from apps.processing.processing_fewsnet import *
 
 #   ---------------------------------------------------------------------
 #   Run the pipeline
 
-processing_modis_sst(**args)
+processing_fewsnet_rfe(**args)
+#processing_ndvi(**args)
