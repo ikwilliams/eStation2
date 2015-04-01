f48dfea98f37a7cda7c84cae291cc1cf2d81e9fd
Mesa User <mesauser@mesa-proc.(none)>
2015-04-01 15:26:01 +0200

    Latest changes

---
+++ apps/processing/processing_modis_sst.py
@@ -0,0 +1,131 @@
+#
+#	purpose: Define the processing service (by using ruffus)
+#	author:  M.Clerici & Jurriaan van't Klooster
+#	date:	 11.06.2014
+#   descr:	 Generate additional derived products / implements processing chains
+#	history: 1.0
+#
+
+# Source my definitions
+from config import es_constants
+import os
+
+# Import eStation2 modules
+#from database import querydb
+from lib.python import functions
+from lib.python import metadata
+from lib.python.image_proc import raster_image_math
+from lib.python.image_proc import recode
+from database import crud
+from lib.python import es_logging as log
+
+# This is temporary .. to be replace with a DB call
+from apps.processing.processing_switches import *
+
+# Import third-party modules
+from ruffus import *
+
+logger = log.my_logger(__name__)
+
+#   General definitions for this processing chain
+prod="fewsnet-rfe"
+mapset='FEWSNET-Africa-8km'
+ext='.tif'
+version='2.0'
+
+#   switch wrt temporal resolution
+activate_10d_comput=1
+
+#   specific switch for each subproduct
+# 1. 10d prod stats
+activate_10davg_comput=1
+activate_10dmin_comput=1
+activate_10dmax_comput=1
+
+def create_pipeline(starting_sprod):
+    #   ---------------------------------------------------------------------
+    #   Define input files
+    in_prod_ident = functions.set_path_filename_no_date(prod, starting_sprod, mapset, version, ext)
+
+    input_dir = es_constants.processing_dir+ \
+                functions.set_path_sub_directory(prod, starting_sprod, 'Ingest', version, mapset)
+
+    starting_files = input_dir+"*"+in_prod_ident
+
+    #   ---------------------------------------------------------------------
+    #   Average
+    output_sprod="10davg"
+    out_prod_ident = functions.set_path_filename_no_date(prod, output_sprod, mapset, version, ext)
+    output_subdir  = functions.set_path_sub_directory   (prod, output_sprod, 'Derived', version, mapset)
+
+    formatter_in="[0-9]{4}(?P<MMDD>[0-9]{4})"+in_prod_ident
+    formatter_out=["{subpath[0][5]}"+os.path.sep+output_subdir+"{MMDD[0]}"+out_prod_ident]
+
+    @active_if(activate_fewsnet_rfe_comput, activate_10d_comput, activate_10davg_comput)
+    @collate(starting_files, formatter(formatter_in),formatter_out)
+    def fewsnet_10davg(input_file, output_file):
+
+        output_file = functions.list_to_element(output_file)
+        functions.check_output_dir(os.path.dirname(output_file))
+        args = {"input_file": input_file, "output_file": output_file, "output_format": 'GTIFF', "options": "compress=lzw"}
+        raster_image_math.do_avg_image(**args)
+
+
+    #   ---------------------------------------------------------------------
+    #   Minimum
+    output_sprod="10dmin"
+    out_prod_ident = functions.set_path_filename_no_date(prod, output_sprod, mapset, version, ext)
+    output_subdir  = functions.set_path_sub_directory   (prod, output_sprod, 'Derived', version, mapset)
+
+    formatter_in="[0-9]{4}(?P<MMDD>[0-9]{4})"+in_prod_ident
+    formatter_out=["{subpath[0][5]}"+os.path.sep+output_subdir+"{MMDD[0]}"+out_prod_ident]
+
+    @active_if(activate_fewsnet_rfe_comput, activate_10d_comput, activate_10dmin_comput)
+    @collate(starting_files, formatter(formatter_in),formatter_out)
+    def fewsnet_10dmin(input_file, output_file):
+
+        output_file = functions.list_to_element(output_file)
+        functions.check_output_dir(os.path.dirname(output_file))
+        args = {"input_file": input_file, "output_file": output_file, "output_format": 'GTIFF', "options": "compress=lzw"}
+        raster_image_math.do_min_image(**args)
+
+    #   ---------------------------------------------------------------------
+    #   Maximum
+    output_sprod="10dmax"
+    out_prod_ident = functions.set_path_filename_no_date(prod, output_sprod, mapset, version, ext)
+    output_subdir  = functions.set_path_sub_directory   (prod, output_sprod, 'Derived', version, mapset)
+
+    formatter_in="[0-9]{4}(?P<MMDD>[0-9]{4})"+in_prod_ident
+    formatter_out=["{subpath[0][5]}"+os.path.sep+output_subdir+"{MMDD[0]}"+out_prod_ident]
+
+    @active_if(activate_fewsnet_rfe_comput, activate_10d_comput, activate_10dmax_comput)
+    @collate(starting_files, formatter(formatter_in),formatter_out)
+    def fewsnet_10dmax(input_file, output_file):
+
+        output_file = functions.list_to_element(output_file)
+        functions.check_output_dir(os.path.dirname(output_file))
+        args = {"input_file": input_file, "output_file": output_file, "output_format": 'GTIFF', "options": "compress=lzw"}
+        raster_image_math.do_max_image(**args)
+
+
+#   ---------------------------------------------------------------------
+#   Run the pipeline
+
+def processing_modis_sst(pipeline_run_level=0,pipeline_run_touch_only=0, pipeline_printout_level=0,
+                           pipeline_printout_graph_level=0):
+
+    create_pipeline(starting_sprod='10d')
+
+    list = pipeline_get_task_names()
+    print list
+    logger.info("Entering routine %s" % 'processing_modis')
+    if pipeline_run_level > 0:
+        logger.info("Now calling pipeline_run")
+        pipeline_run(verbose=pipeline_run_level, touch_files_only=pipeline_run_touch_only)
+    
+    if pipeline_printout_level > 0:
+        
+        pipeline_printout(verbose=pipeline_printout_level)
+    
+    if pipeline_printout_graph_level > 0:
+        pipeline_printout_graph('flowchart.jpg')
